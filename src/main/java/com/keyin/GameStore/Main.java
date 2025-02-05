97914fc383facc4001b4f290ca0cce21d1c63351
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-05 19:13:52 -0330

    Basic menu structure

--- src/main/java/com/keyin/GameStore/Main.java
+++ src/main/java/com/keyin/GameStore/Main.java
@@ -1,19 +1,41 @@
 package com.keyin.GameStore;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Store store = new Store();
+        Scanner scanner = new Scanner(System.in);
+        boolean appOn = true;
 
-        // Three best video games ever made (fight me)
-        Game game1 = new Game(1, "Bloodborne", "Action/Souls Like", 24.99);
-        Game game2 = new Game(2, "Baldurs Gate 3", "Isometric RPG", 69.99);
-        Game game3 = new Game(3, "Alan Wake 2", "Survival Horror", 49.99);
+        System.out.println("Welcome to Brandon's Roadside Game-bar!");
 
-        store.addGame(game1);
-        store.addGame(game2);
-        store.addGame(game3);
+        while (appOn)
+        {
+            System.out.println("Menu:");
+            System.out.println("1. View available games");
+            System.out.println("2. Add new game");
+            System.out.println("3. Purchase a game");
+            System.out.println("4. Exit");
+            System.out.print("Choose option!");
 
-        store.displayGames();
+            int choice = scanner.nextInt();
+
+
+        }
+
+
+
+//        // Three best video games ever made (fight me)
+//        Game game1 = new Game(1, "Bloodborne", "Action/Souls Like", 24.99);
+//        Game game2 = new Game(2, "Baldurs Gate 3", "Isometric RPG", 69.99);
+//        Game game3 = new Game(3, "Alan Wake 2", "Survival Horror", 49.99);
+//
+//        store.addGame(game1);
+//        store.addGame(game2);
+//        store.addGame(game3);
+//
+//        store.displayGames();
     }
 }
